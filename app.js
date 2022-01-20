a8ed6b3584998285961dd219665e1e1d4eb10dbb
AlexDDks <magarciaa92@gmail.com>
2022-01-19 19:11:00 -0600

    Subiendo login

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html')
+})
+
+app.listen(3000, () => {
+    console.log('Servidor funcionando')
+})
